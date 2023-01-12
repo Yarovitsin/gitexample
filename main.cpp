ce443951888577a57d009a7344bf9119879d581b
Vanya Iarovitzine <ivphoneac@gmail.com>
2023-01-12 17:06:11 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int maxOfArray(int array[], int size) {
+    int max = array[0];
+    for (int i = 1; i < size; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+
+}
