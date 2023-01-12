f22b35a13544de94d2cf0423e5b6928467a7a251
Vanya Iarovitzine <ivphoneac@gmail.com>
2023-01-12 17:19:25 +0300

    Array input added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+using namespace std;
+
 int maxOfArray(int array[], int size) {
     int max = array[0];
     for (int i = 1; i < size; i++) {
@@ -10,6 +12,15 @@ int maxOfArray(int array[], int size) {
     return max;
 }
 
-int main() {
+int* inputArray(int size) {
+    int array[size];
+    for (int i = 0; i < size; i++) {
+        cin >> array[i];
+    }
+    return array;
+}
 
+int main() {
+    cout << maxOfArray(inputArray(5), 5);
+    return 0;
 }
